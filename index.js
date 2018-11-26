8a1f64ad9df1da29dd74acb357eee09722005eeb
zenryoku <zenryoku@hotmail.com>
2018-11-26 03:01:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
 
-function destructivelyRemoveLastKitten(name) {
-  
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name);
 }
